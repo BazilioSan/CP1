a039f66bdae60c8c78ee2cce3210082871d25bf1
BazilioSan <blix@list.ru>
2024-08-11 20:46:46 +0300

    vol.1.15

--- src/main.py
+++ src/main.py
@@ -7,10 +7,13 @@ import pandas as pd
 
 if __name__ == "__main__":
     date = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d %H:%M:%S")
-    print(views(date))
-    print("\n###################\n")
     transactions_list = reading_file_from_excel("operations.xls")
+    transactions_df = pd.DataFrame(transactions_list)
+
+    print(views(date, transactions_df))
+    print("\n###################\n")
+
     print(investment_bank("2021-10", transactions_list.to_dict(orient="records"), 100))
     print("\n###################\n")
-    transactions_df = pd.DataFrame(transactions_list)
+
     print((spent_by_category(transactions_df, "Фастфуд", "2021-10-25")).head())
